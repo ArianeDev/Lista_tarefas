ca296241fd9f8e863c6fc43dd4473e8e1863fba0
ArianeDev <silvaarianeoliveira2@gmail.com>
2026-01-13 21:42:50 -0300

    Foi acrescentado novos componentes e a organização deles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,32 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { ArrowDownRight, Check, List, Plus } from "lucide-react";
 
 const Home = () => {
   return (
-    <div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Home</CardTitle>
-          <CardDescription>Ola</CardDescription>
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg">
+        <CardHeader className="flex gap-2">
+          <Input placeholder="Adicionar tarefa"/>
+          <Button className="cursor-pointer"> 
+            <Plus />
+            Cadastrar
+          </Button>
         </CardHeader>
-        <CardFooter>
-          <Button>Click me</Button>
-          <Input /> 
-        </CardFooter>
+        
+        <CardContent>
+          <Separator />
+          <div className="flex gap-2">
+            <Badge className="cursor-pointer"><List />Todas</Badge>
+            <Badge className="cursor-pointer"><ArrowDownRight />Não finalizado</Badge>
+            <Badge className="cursor-pointer"><Check />Concluídas</Badge>
+          </div>
+        </CardContent>
       </Card>
-    </div>
+    </main>
   )
 }
 
